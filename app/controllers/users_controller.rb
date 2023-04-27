139a9e2eae182030ef8361d86c8ea49a2c53bafd
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-27 14:25:56 -0500

    add function to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,48 @@
 class UsersController < ApplicationController
-
   def index
-    @users = User.all
-    render json: {message: "Working!"}
+    @user = User.all
+    render :index
   end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+    render :show
+  end  
 
+  def create
+    @user = User.new(
+      name: params[:name], 
+      email: params[:age], 
+      password: params[:password]
+    )
+
+    if @user.save
+      render :show
+    else
+      render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+  
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.password = params[:password] || @user.password
+  
+    
+    if @user.save
+      render :show
+    else
+      render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+    end
+    
+  end
+  
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: {message: "User has been successfully removed"}
+  end
 
 end
