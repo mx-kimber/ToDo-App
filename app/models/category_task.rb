d4b43741dda9b4cd60c3f1360cf42c2283bf4508
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-27 15:47:09 -0500

    add CategoryTasks table and 2 attributes userid and categoryid

---
+++ app/models/category_task.rb
@@ -0,0 +1,2 @@
+class CategoryTask < ApplicationRecord
+end
--- db/migrate/20230427200646_add_image_url_to_categories_table.rb
+++ db/migrate/20230427200646_add_image_url_to_categories_table.rb
@@ -1,4 +1,5 @@
 class AddImageUrlToCategoriesTable < ActiveRecord::Migration[7.0]
   def change
+  add_column :categories, :image_url, :string
   end
 end
