d7ad2acf425f424a02e70a7fda63b1aba8cbf681
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-27 13:13:03 -0500

    add create controller action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,15 @@
 class CategoriesController < ApplicationController
 
-def index 
-  @categories = Category.all 
-  render json:{message: "Is this working?"}
-end 
+  def index 
+    @categories = Category.all 
+    render json:{message: "Is this working?"}
+  end 
+
+  def create
+    @category = Category.create( 
+      name: params[:name],
+    )
+    render :show
+  end 
 
 end
